217e9b8118cd1800dd6857ad07f842fa115836a8
raianni92-coder <raianni92@gmail.com>
2026-02-13 09:50:44 +0530

    Add files via upload

---
+++ ircumference.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float r,a,c;
+    printf("enter r:");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("area:%f",a);
+    c=2*3.14*r;
+    printf("\ncircumference:%f",c);
+    return 0;
+    
+}
